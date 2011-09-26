58da6a12424033dd901a769c683f3d0a37bd77fc
Steve Madsen <steve@lightyearsoftware.com>
2011-09-26 11:57:09 -0400

    New method aliases for ActiveRecord 3.1.
    ActiveRecord 3.1.0 calls exec_query and exec_delete instead of execute
    in most cases. I didn't do extensive research, but this might be related
    to prepared statement caching added in 3.1.

--- lib/sql-logging/adapters/postgresql.rb
+++ lib/sql-logging/adapters/postgresql.rb
@@ -15,5 +15,31 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
     result
   end
   
+  def exec_query_with_sql_logging(sql, *args)
+    result = nil
+    elapsed = Benchmark.measure do
+      result = exec_query_without_sql_logging(sql, *args)
+    end
+    msec = elapsed.real * 1000
+    if result.respond_to?(:rows)
+      SqlLogging::Statistics.record_query(sql, args.first, msec, result.rows)
+    else
+      SqlLogging::Statistics.record_query(sql, args.first, msec, result)
+    end
+    result
+  end
+  
+  def exec_delete_with_sql_logging(sql, *args)
+    result = nil
+    elapsed = Benchmark.measure do
+      result = exec_delete_without_sql_logging(sql, *args)
+    end
+    msec = elapsed.real * 1000
+    SqlLogging::Statistics.record_query(sql, args.first, msec, result)
+    result
+  end
+  
   alias_method_chain :execute, :sql_logging
+  alias_method_chain :exec_query, :sql_logging
+  alias_method_chain :exec_delete, :sql_logging
 end
