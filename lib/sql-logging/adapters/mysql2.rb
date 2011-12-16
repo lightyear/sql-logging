53c9882fafca34dbcc324fec2d11dd53d2fa743f
gorn <jakub.cz.gsub(/a/,@)>
2011-12-16 05:14:01 +0100

    adding third parameter to select to comply with 3.1.3

--- lib/sql-logging/adapters/mysql2.rb
+++ lib/sql-logging/adapters/mysql2.rb
@@ -1,10 +1,10 @@
 require 'active_record/connection_adapters/mysql2_adapter'
 
 class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  def select_with_sql_logging(sql, name = nil)
+  def select_with_sql_logging(sql, name = nil, binds = [])
     rows = nil
     elapsed = Benchmark.measure do
-      rows = select_without_sql_logging(sql, name)
+      rows = select_without_sql_logging(sql, name, binds)
     end
     msec = elapsed.real * 1000
     SqlLogging::Statistics.record_query(sql, name, msec, rows)
