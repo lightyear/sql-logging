lightyear/sql-logging

commit 5bb0111239894b5e3e367348a4f3a4ab7093b0ec

PostgreSQL #select doesn't (yet) need the *args workaround.

lib/sql-logging/adapters/postgresql.rb
@@ -4,13 +4,13 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
   def execute_with_sql_logging(sql, *args)
     result = nil
     elapsed = Benchmark.measure do
-      result = execute_without_sql_logging(sql, *args)
+      result = execute_without_sql_logging(sql, name)
     end
     msec = elapsed.real * 1000
     if result.respond_to?(:rows)
-      SqlLogging::Statistics.record_query(sql, args.first, msec, result.rows)
+      SqlLogging::Statistics.record_query(sql, name, msec, result.rows)
     else
-      SqlLogging::Statistics.record_query(sql, args.first, msec, result)
+      SqlLogging::Statistics.record_query(sql, name, msec, result)
     end
     result
   end

commit 40c3a2e8b4d522b8c077a2ff7a0b88593b40180d

Fix for ActiveRecord 3.2.
I broke this in 5bb01112398 when I reverted the *args change for AR 3.1 in just one method for the PostgreSQL adapter, but then forgot to revert the change to the argument list, too.

lib/sql-logging/adapters/postgresql.rb
@@ -4,13 +4,13 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
   def execute_with_sql_logging(sql, *args)
     result = nil
     elapsed = Benchmark.measure do
-      result = execute_without_sql_logging(sql, name)
+      result = execute_without_sql_logging(sql, *args)
     end
     msec = elapsed.real * 1000
     if result.respond_to?(:rows)
-      SqlLogging::Statistics.record_query(sql, name, msec, result.rows)
+      SqlLogging::Statistics.record_query(sql, args.first, msec, result.rows)
     else
-      SqlLogging::Statistics.record_query(sql, name, msec, result)
+      SqlLogging::Statistics.record_query(sql, args.first, msec, result)
     end
     result
   end