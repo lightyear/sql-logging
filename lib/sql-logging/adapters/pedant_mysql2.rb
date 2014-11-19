c6958f967ca30cd4fb7367662abba06ac7217adf
Thierry Joyal <thierry.joyal@gmail.com>
2014-11-19 11:39:48 -0500

    Add pedant_mysql2 adapter

---
+++ lib/sql-logging/adapters/pedant_mysql2.rb
@@ -0,0 +1,15 @@
+require 'active_record/connection_adapters/pedant_mysql2_adapter'
+
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  def select_with_sql_logging(sql, *args)
+    rows = nil
+    elapsed = Benchmark.measure do
+      rows = select_without_sql_logging(sql, *args)
+    end
+    msec = elapsed.real * 1000
+    SqlLogging::Statistics.record_query(sql, args.first, msec, rows)
+    rows
+  end
+
+  alias_method_chain :select, :sql_logging
+end
